2b84f8866d1569d3fc76c5a5a720fa735f46b968
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2017-02-22 19:43:09 +0530

    3 auth_users.html modified

--- source/PyApp/urls.py
+++ source/PyApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
  		url(r'^register/$', 'PyApp.views.registration', name='registration'),	
         url(r"^image-posts/$",'PyApp.views.image_posts',name='image_posts'),
         url(r"^auth-users/$","PyApp.views.auth_users",name="auth_users"),
+        url(r"^order-by-firstname/$","PyApp.views.order_by_firstname",name="order_by_firstname"),
     ] 
--- source/PyApp/views.py
+++ source/PyApp/views.py
@@ -12,9 +12,14 @@ def registration(request):
 		form.save(commit=True)
 	return render(request,"registration.html",{"form":form})
 
+def order_by_firstname(request):
+	users=AuthUser.objects.all().order_by("firstname");
+	return render(request,"auth_users.html",{"users":users,"order":"Order by firstname"})
+
+
 def auth_users(request):
 	users=AuthUser.objects.all()
-	return render(request,"auth_users.html",{"users":users})
+	return render(request,"auth_users.html",{"users":users,"order":"Order by created_at"})
 
 def image_posts(request):
 	return render(request,"image_posts.html",{})
